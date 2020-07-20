fdae53054d153d1f3cfba3266c013ed2bde657b5
Rafal Skrzydelski <r.skrzydelski@gmail.com>
2020-07-20 10:15:33 +0200

    [UI] Refactor api requests for market

--- src/components/pages/Market.js
+++ src/components/pages/Market.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Axios from 'axios'
 
 import { gold999ozUrl, gold585gUrl, silver999ozUrl, silver800gUrl, accountUrl } from '../endpoints'
-import { get } from '../api'
 
 import gold_ico from '../../static/gold_ico.png'
 import gold585_ico from '../../static/gold_585_ico.png'
@@ -32,58 +32,51 @@ export default class Market extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            market: {
-                gold999oz: '',
-                gold585g: '',
-                silver999oz: '',
-                silver800g: ''
-            },
+          market: {
+            gold999oz: '',
+            gold585g: '',
+            silver999oz: '',
+            silver800g: ''
+          },
             my_currency: ''
         }
-        this.getMarketData()
-        get(accountUrl, this.getCurrency)
       }
 
-  getCurrency = (data) => {
-    if (data.my_currency === 'PLN') {
-      this.setState({my_currency: 'zł'})
-      } else if (data.my_currency === 'USD') {
-        this.setState({my_currency: '$'})
+  getCurrency = async () => {
+    try {
+        const res = await Axios.get(accountUrl, {headers: {authorization: 'JWT ' + localStorage.getItem('access')}})
+        if (res.data.my_currency === 'PLN') {
+          this.setState({my_currency: 'zł'})
+        } else if (res.data.my_currency === 'USD') {
+          this.setState({my_currency: '$'})
       }
+    } catch (error) {
+
+    }
   }
 
-  collectMarketGold999 = (data) => {
-    var market = {...this.state.market}
-    market.gold999oz = data.price;
-    this.setState({market})
-  }
-
-  collectMarketGold585 = (data) => {
-    var market = {...this.state.market}
-    market.gold585g = data.price;
-    this.setState({market})
-  }
-
-  collectMarketSilver999 = (data) => {
-    var market = {...this.state.market}
-    market.silver999oz = data.price;
-    this.setState({market})
-  }
-
-  collectMarketSilver800 = (data) => {
-    var market = {...this.state.market}
-    market.silver800g = data.price;
-    this.setState({market})
-  }
-
-  getMarketData = () => {
-    get(gold999ozUrl, this.collectMarketGold999)
-    get(gold585gUrl, this.collectMarketGold585)
-    get(silver999ozUrl, this.collectMarketSilver999)
-    get(silver800gUrl, this.collectMarketSilver800)
+  getMarketData = async () => {
+    try {
+      const gold999Promise = Axios(gold999ozUrl, {headers: {authorization: 'JWT ' + localStorage.getItem('access')}})
+      const gold585Promise = Axios(gold585gUrl, {headers: {authorization: 'JWT ' + localStorage.getItem('access')}})
+      const silver999Promise = Axios(silver999ozUrl, {headers: {authorization: 'JWT ' + localStorage.getItem('access')}})
+      const silver800Promise = Axios(silver800gUrl, {headers: {authorization: 'JWT ' + localStorage.getItem('access')}})
+  
+      const res = await Promise.all([gold999Promise, gold585Promise, silver999Promise, silver800Promise])
+      var market = {...this.state.market}
+      market.gold999oz = res[0].data.price
+      market.gold585g = res[1].data.price
+      market.silver999oz = res[2].data.price
+      market.silver800g = res[3].data.price
+      this.setState({market})
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   componentDidMount () {
+    this.getMarketData()
+    this.getCurrency()
     const timer = 60 * 1000
     this.myInterval = setInterval(this.getMarketData, timer)
   }
